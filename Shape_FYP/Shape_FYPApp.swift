cd99f28e7a615559440944d1a634b1efb8455b18
Mayho <twwong2001@yahoo.com.hk>
2024-03-10 22:42:42 +0800

    Initial Commit

---
+++ Shape_FYP/Shape_FYPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shape_FYPApp.swift
+//  Shape_FYP
+//
+//  Created by Evan Wong on 10/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct Shape_FYPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shape_FYPUITests/Shape_FYPUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Shape_FYPUITestsLaunchTests.swift
+//  Shape_FYPUITests
+//
+//  Created by Evan Wong on 10/3/2024.
+//
+
+import XCTest
+
+final class Shape_FYPUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
